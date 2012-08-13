035adedf242ca853b4b5343e9870db59defe007d
mbenson99 <mbenson99@gmail.com>
2012-08-12 20:22:56 -0400

    this is 'broken' but very close

--- javascript/LocationServices.js
+++ javascript/LocationServices.js
@@ -1,3 +1,7 @@
+var serviceURL = "http://brodownandcrunchcode.com/Location/";
+var radius = "1000"
+var events;
+
 function popup() {
     alert("Hello World")
 } 
@@ -6,7 +10,7 @@ function popup() {
 //   This method accepts a `Position` object, which contains
 //   the current GPS coordinates
 //
-function onSuccess(position) {
+function onSuccess2(position) {
     var element = document.getElementById('geolocation');
     element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
     'Longitude: '          + position.coords.longitude             + '<br />' +
@@ -20,6 +24,49 @@ function onSuccess(position) {
     
 }
 
+
+
+
+function onSuccess(position){
+    var service = serviceURL + position.coords.latitude.toString() + "/" + position.coords.longitude.toString() + "/" + radius;
+    alert(service)
+
+
+    $.getJSON(service,function(data){
+        
+        //alert(data)
+        //alert(data.item)
+        
+        $('#eventList li').remove();
+        
+        events = data.items;
+        var stuff = "";
+        $.each(events, function(index,event){
+            stuff = event.VenueName
+            $('#eventList').append('<li>' +'Venue: ' + event.VenueName + ', ' + event.City +'</li>');
+        });
+        alert(stuff)
+        
+        $('#eventList').listview('refresh');
+    
+    })
+    .success(function(){alert("success");})
+    .error(function(jqXHR, textStatus, errorThrown){
+        alert("error " + textStatus);
+        alert("incoming text " + jqXHR.responseText);
+    })
+    .complete(function(){alert("complete");});
+}
+
+function getLocation(){
+    
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
+
+$('#eventListPage').bind('pageinit',function(event){
+    
+    getLocation(); 
+});
 
 // onError Callback receives a PositionError object
 //
